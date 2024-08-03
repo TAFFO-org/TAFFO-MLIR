615429c14838f498fd449be564221f1cf588d53d
Paolo6Galli <paolo.galli@mail.polimi.it>
2024-08-03 18:06:02 +0200

    Added AddOp conversion

--- lib/Taffo/Transforms/LowerToArithPass.cpp
+++ lib/Taffo/Transforms/LowerToArithPass.cpp
@@ -1,29 +1,80 @@
-#include "Taffo/Dialect/Taffo.h"
 #include "Taffo/Transforms/LowerToArithPass.h"
 #include "Taffo/Dialect/Attributes.h"
+#include "Taffo/Dialect/Taffo.h"
 #include "mlir/IR/Visitors.h"
 #include "mlir/Pass/Pass.h"
 
 #include "Taffo/Dialect/Ops.h"
 
-namespace mlir::taffo
-{
+namespace mlir::taffo {
 #define GEN_PASS_DEF_LOWERTOARITHPASS
 #include "Taffo/Transforms/Passes.h.inc"
-}
+} // namespace mlir::taffo
 
 using namespace ::mlir::taffo;
 
 namespace {
 class LowerToArithPass
-    : public mlir::taffo::impl::LowerToArithPassBase<
-          LowerToArithPass> {
+    : public mlir::taffo::impl::LowerToArithPassBase<LowerToArithPass> {
 public:
   using LowerToArithPassBase::LowerToArithPassBase;
 
-  void runOnOperation() override {
-    mlir::Operation *module = getOperation();
+  class TaffoToArithTypeConverter : public TypeConverter {
+  public:
+    TaffoToArithTypeConverter(MLIRContext *ctx, int bitwidth) {
+      addConversion([](Type type) { return type; });
+      addConversion([ctx](RealType type) -> Type {
+        return IntegerType::get(ctx, bitwidth,
+                                IntegerType::SignednessSemantics::Signless);
+      });
+    }
+  };
 
-  }
+  struct ConvertAdd : public OpConversionPattern<AddOp> {
+    ConvertAdd(mlir::MLIRContext *context)
+        : OpConversionPattern<AddOp>(context) {}
+
+    using OpConversionPattern::OpConversionPattern;
+
+    LogicalResult
+    matchAndRewrite(AddOp op, OpAdaptor adaptor,
+                    ConversionPatternRewriter &rewriter) const override {
+
+      ImplicitLocOpBuilder b(op.getLoc(), rewriter);
+
+      int getExp = [](Value v) {
+        return v.getDefinigOp().getAttr("DatatypeInfo").getExp();
+      };
+      int rhsExp = getExp(adaptor.getRhs());
+      int lhsExp = getExp(adaptor.getLhs());
+
+
+      int expDiff = std::abs(rhsExp - lhsExp);
+      if (expDiff > 32) {
+        // TODO if difference between exps is greater than bitwidth, delete
+        //  op with warning
+      }
+      if (expDiff != 0) {
+        Value to_shift = rhsExp < lhsExp ? adaptor.getRhs() : adaptor.getLhs();
+        Value no_shift = !rhsExp < lhsExp ? adaptor.getRhs() : adaptor.getLhs();
+
+        arith::ConstantOp shift_amount =
+            b.create<arith::ConstantOp>(b.getI32IntegerAttr(expDiff));
+        arith::ShRSIOp ShOp =
+            b.create<arith::ShRSIOp>(to_shift, shift_amount.getResult());
+        arith::AddIOp addOp =
+            b.create<arith::AddIOp>(no_shift, ShOp.getResult());
+        rewriter.replaceOp(op.getOperation(), addOp);
+        return success();
+      }
+
+      arith::AddIOp addOp =
+          b.create<arith::AddIOp>(adaptor.getLhs(), adaptor.getRhs());
+      rewriter.replaceOp(op.getOperation(), addOp);
+      return success();
+    }
+  };
+
+  void runOnOperation() override { mlir::Operation *module = getOperation(); }
 };
-}
+} // namespace
