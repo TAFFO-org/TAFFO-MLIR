06fdd15a3dcd968ad726539147d589fcaa08a238
Paolo6Galli <paolo.galli@mail.polimi.it>
2024-08-05 19:22:54 +0200

    lack of cooperation by c++ lambdas

--- lib/Taffo/Transforms/LowerToArithPass.cpp
+++ lib/Taffo/Transforms/LowerToArithPass.cpp
@@ -1,8 +1,10 @@
 #include "Taffo/Transforms/LowerToArithPass.h"
 #include "Taffo/Dialect/Attributes.h"
 #include "Taffo/Dialect/Taffo.h"
+#include "mlir/IR/ImplicitLocOpBuilder.h"
 #include "mlir/IR/Visitors.h"
 #include "mlir/Pass/Pass.h"
+#include "mlir/Transforms/DialectConversion.h"
 
 #include "Taffo/Dialect/Ops.h"
 
@@ -13,17 +15,17 @@ namespace mlir::taffo {
 
 using namespace ::mlir::taffo;
 
-namespace {
+namespace mlir {
 class LowerToArithPass
     : public mlir::taffo::impl::LowerToArithPassBase<LowerToArithPass> {
 public:
   using LowerToArithPassBase::LowerToArithPassBase;
 
-  class TaffoToArithTypeConverter : public TypeConverter {
+  class TaffoToArithTypeConverter : public mlir::TypeConverter {
   public:
     TaffoToArithTypeConverter(MLIRContext *ctx, int bitwidth) {
       addConversion([](Type type) { return type; });
-      addConversion([ctx](RealType type) -> Type {
+      addConversion([ctx, bitwidth](RealType type) -> Type {
         return IntegerType::get(ctx, bitwidth,
                                 IntegerType::SignednessSemantics::Signless);
       });
@@ -42,21 +44,35 @@ public:
 
       ImplicitLocOpBuilder b(op.getLoc(), rewriter);
 
-      int getExp = [](Value v) {
-        return v.getDefinigOp().getAttr("DatatypeInfo").getExp();
+      std::optional<int> getExp = [](Value v) {
+        DatatypeInfoAttr attr = v.getDefiningOp()
+                                    ->getAttr("DatatypeInfo")
+                                    .dyn_cast_or_null<DatatypeInfoAttr>();
+        return (attr != NULL) ? std::optional<int>(attr.getExponent())
+                              : std::nullopt;
       };
-      int rhsExp = getExp(adaptor.getRhs());
-      int lhsExp = getExp(adaptor.getLhs());
 
+      std::optional<int> rhsExp = getExp(adaptor.getRhs());
+      if (!rhsExp.has_value) {
+        op->emitOpError() << "DatatypeInfo has not been set for rhs";
+        return failure()
+      }
+      std::optional<int> lhsExp = getExp(adaptor.getLhs());
+      if (!lhsExp.has_value) {
+        op->emitOpError() << "DatatypeInfo has not been set for lhs";
+        return failure()
+      }
 
-      int expDiff = std::abs(rhsExp - lhsExp);
+      int expDiff = std::abs(rhsExp.value - lhsExp.value);
       if (expDiff > 32) {
         // TODO if difference between exps is greater than bitwidth, delete
         //  op with warning
       }
       if (expDiff != 0) {
-        Value to_shift = rhsExp < lhsExp ? adaptor.getRhs() : adaptor.getLhs();
-        Value no_shift = !rhsExp < lhsExp ? adaptor.getRhs() : adaptor.getLhs();
+        Value to_shift =
+            rhsExp.value < lhsExp.value ? adaptor.getRhs() : adaptor.getLhs();
+        Value no_shift =
+            !rhsExp.value < lhsExp.value ? adaptor.getRhs() : adaptor.getLhs();
 
         arith::ConstantOp shift_amount =
             b.create<arith::ConstantOp>(b.getI32IntegerAttr(expDiff));
@@ -75,6 +91,21 @@ public:
     }
   };
 
-  void runOnOperation() override { mlir::Operation *module = getOperation(); }
+  void runOnOperation() override {
+    MLIRContext *context = &getContext();
+    mlir::Operation *module = getOperation();
+
+    ConversionTarget target(*context);
+    target.addLegalDialect<arith::ArithDialect>();
+    // target.addIllegalDialect<TaffoDialect>();
+
+    RewritePatternSet patterns(context);
+    TaffoToArithTypeConverter typeConverter(context, 32);
+    patterns.add<ConvertAdd>(typeConverter, context);
+
+    if (failed(applyPartialConversion(module, target, std::move(patterns)))) {
+      signalPassFailure();
+    }
+  }
 };
-} // namespace
+} // namespace mlir
