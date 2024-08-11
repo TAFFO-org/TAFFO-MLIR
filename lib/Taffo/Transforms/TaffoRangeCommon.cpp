8c704501db5be0c95869e458856620a6235bd721
Paolo6Galli <paolo.galli@mail.polimi.it>
2024-08-11 13:00:58 +0200

    Added assertions

--- lib/Taffo/Transforms/TaffoRangeCommon.cpp
+++ lib/Taffo/Transforms/TaffoRangeCommon.cpp
@@ -4,39 +4,42 @@ using namespace ::llvm;
 
 namespace mlir::taffo {
 
-
-  template<>
-  // unused (for now?)
-  NtvRange inferCast(ArrayRef<NtvRange> argRanges) {
-    assert(argRanges[0].first <= argRanges[0].second);
-    return argRanges[0];
-  }
-
-  template<>
-  NtvRange inferConvert(ArrayRef<NtvRange> argRanges) {
-    assert(argRanges[0].first <= argRanges[0].second);
-    return argRanges[0];
-  }
-
-  template<>
-  NtvRange inferAdd(ArrayRef<NtvRange> argRanges) {
-    assert(argRanges[0].first <= argRanges[0].second);
-    assert(argRanges[1].first <= argRanges[1].second);
-
-    APFloat min = argRanges[0].first + argRanges[1].first;
-    APFloat max = argRanges[0].second + argRanges[1].second;
-
-    return NtvRange(min, max);
-  }
-
-  template<>
-  NtvRange inferMult(ArrayRef<NtvRange> argRanges) {
-    // unrolled outer product
-    APFloat a0b0 = argRanges[0].first * argRanges[1].first;
-    APFloat a0b1 = argRanges[0].first * argRanges[1].second;
-    APFloat a1b0 = argRanges[0].second * argRanges[1].first;
-    APFloat a1b1 = argRanges[0].second * argRanges[1].second;
-
-    return std::minmax({a0b0, a0b1, a1b0, a1b1});
-  }
+template <>
+// unused (for now?)
+NtvRange inferCast(ArrayRef<NtvRange> argRanges) {
+  assert(argRanges[0].first <= argRanges[0].second &&
+         "Upperbound and lowerbound of this range are inverted");
+  return argRanges[0];
 }
+
+template <>
+NtvRange inferConvert(ArrayRef<NtvRange> argRanges) {
+  assert(argRanges[0].first <= argRanges[0].second &&
+         "Upperbound and lowerbound of this range are inverted");
+  return argRanges[0];
+}
+
+template <>
+NtvRange inferAdd(ArrayRef<NtvRange> argRanges) {
+  assert(argRanges[0].first <= argRanges[0].second &&
+         "Upperbound and lowerbound of this range are inverted");
+  assert(argRanges[1].first <= argRanges[1].second  &&
+         "Upperbound and lowerbound of this range are inverted");
+
+  APFloat min = argRanges[0].first + argRanges[1].first;
+  APFloat max = argRanges[0].second + argRanges[1].second;
+
+  return NtvRange(min, max);
+}
+
+template <>
+NtvRange inferMult(ArrayRef<NtvRange> argRanges) {
+  // unrolled outer product
+  APFloat a0b0 = argRanges[0].first * argRanges[1].first;
+  APFloat a0b1 = argRanges[0].first * argRanges[1].second;
+  APFloat a1b0 = argRanges[0].second * argRanges[1].first;
+  APFloat a1b1 = argRanges[0].second * argRanges[1].second;
+
+  return std::minmax({a0b0, a0b1, a1b0, a1b1});
+}
+} // namespace mlir::taffo
