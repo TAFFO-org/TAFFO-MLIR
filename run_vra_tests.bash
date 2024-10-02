4c26a7e236e71d71997adb4f2065571b36f78811
MH2033 <aakbarimh@gmail.com>
2024-10-02 10:35:27 +0200

    Add sctipt runner

---
+++ run_vra_tests.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd build
+ninja
+cd bin
+
+set -x
+
+for i in {1..5}
+do 
+    ./taffo-opt -pass-pipeline="builtin.module(value-range-analysis, dt-optimization, lower-to-arith, convert-func-to-llvm)" ./../../test/Taffo/test$i.mlir -debug-only=value-range-analysis | mlir-cpu-runner -e=simple_constant-entry-point-result=f32
+
+done
