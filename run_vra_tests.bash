119d6d9f5d5fc5bb19b461e713f7049af325d80b
MH2033 <aakbarimh@gmail.com>
2024-10-19 10:15:29 +0200

    Add bash script for running all the tests

--- run_vra_tests.bash
+++ run_vra_tests.bash
@@ -6,8 +6,8 @@ cd bin
 
 set -x
 
-for i in {1..5}
+for i in {1..6}
 do 
-    ./taffo-opt -pass-pipeline="builtin.module(value-range-analysis, dt-optimization, lower-to-arith, convert-func-to-llvm)" ./../../test/Taffo/test$i.mlir -debug-only=value-range-analysis | mlir-cpu-runner -e=simple_constant-entry-point-result=f32
+    ./taffo-opt -pass-pipeline="builtin.module(value-range-analysis, dt-optimization, lower-to-arith, convert-func-to-llvm)" ./../../test/Taffo/test$i.mlir -debug-only=value-range-analysis | mlir-cpu-runner -e=simple_constant -entry-point-result=f32
 
 done
