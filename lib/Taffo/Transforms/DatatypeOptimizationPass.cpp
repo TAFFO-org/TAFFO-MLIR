ebd6997a8cb174a4024d74613c5be4fbcbf4f400
Paolo6Galli <paolo.galli@mail.polimi.it>
2024-10-23 19:11:22 +0200

    removed overflow check on dt-optimization as it's reduntant wrt VRA

--- lib/Taffo/Transforms/DatatypeOptimizationPass.cpp
+++ lib/Taffo/Transforms/DatatypeOptimizationPass.cpp
@@ -59,54 +59,6 @@ public:
     if (result.wasInterrupted())
       signalPassFailure();
 
-    // This manipulation on users addOp is necessary to prevent overflow. This
-    // can be achieved in different parts of the pipeline if the need arises in
-    // the future. It relies on the fact that all taffo ops are converting to
-    // the same bitwidth, as this has relevance wrt to exponent semantics and
-    // fixed-point alignment
-//    auto result2 = module->walk([&](mlir::Operation *op) {
-//      if (llvm::none_of(op->getResultTypes(),
-//                        [](Type t) { return llvm::isa<RealType>(t); })) {
-//        return mlir::WalkResult::advance();
-//      }
-//
-//      auto possibleOverflow = [op](Operation *addOp) {
-//        if (!llvm::isa<taffo::AddOp>(addOp)) {
-//          return false;
-//        }
-//        int childExp = ::llvm::dyn_cast<RealType>(addOp->getResult(0).getType())
-//                           .getExponent();
-//
-//        int parentExp = ::llvm::dyn_cast<RealType>(op->getResult(0).getType())
-//                            .getExponent();
-//
-//        return childExp == (parentExp + 1);
-//      };
-//
-//      auto usersWithOF =
-//          llvm::make_filter_range(op->getUsers(), possibleOverflow);
-//
-//      RealType oldType = ::llvm::dyn_cast<RealType>(op->getResult(0).getType());
-//
-//      RealType newType =
-//          RealType::get(op->getContext(), oldType.getSignd(),
-//                        oldType.getExponent() + 1, oldType.getBitwidth());
-//      mlir::OpBuilder b = mlir::OpBuilder(op, nullptr);
-//      auto align = b.create<mlir::taffo::AlignOp>(op->getLoc(), newType,
-//                                                  op->getResult(0));
-//
-//      op->getResult(0).replaceUsesWithIf(
-//          align.getResult(), [usersWithOF](mlir::OpOperand &U) {
-//            return llvm::any_of(usersWithOF, [&](Operation *user) {
-//              return user == U.getOwner();
-//            });
-//          });
-//
-//      return mlir::WalkResult::advance();
-//    });
-    module->dump();
-//    if (result2.wasInterrupted())
-//      signalPassFailure();
   }
 };
 } // namespace mlir
