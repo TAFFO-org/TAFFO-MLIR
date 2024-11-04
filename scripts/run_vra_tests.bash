6ce8db476dc7b37b49a6128202539b81905b989c
MH2033 <aakbarimh@gmail.com>
2024-11-04 15:02:09 +0100

    Run all tests

--- scripts/run_vra_tests.bash
+++ scripts/run_vra_tests.bash
@@ -8,6 +8,6 @@ set -x
 
 for i in {1..7}
 do 
-    ./taffo-opt -pass-pipeline="builtin.module(value-range-analysis)" ./../../test/Taffo/test7.mlir -debug-only=value-range-analysis
+    ./taffo-opt -pass-pipeline="builtin.module(value-range-analysis)" ./../../test/Taffo/test$i.mlir -debug-only=value-range-analysis
 
 done
