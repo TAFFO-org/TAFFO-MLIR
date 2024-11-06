3e6abf107b419ab24cd1532fceefd00c086c8acb
MH2033 <aakbarimh@gmail.com>
2024-11-06 11:51:18 +0100

    Changed the definition of equality

--- lib/Taffo/Transforms/libaffine.cpp
+++ lib/Taffo/Transforms/libaffine.cpp
@@ -11,12 +11,16 @@ Var operator*(const llvm::APFloat b, const Var var) { return var * b; }
 bool operator==(const Var &a, const Var &b) {
   if (a.c_value.isNaN() && b.c_value.isNaN())
     return true;
-  if (a.c_value != b.c_value)
-    return false;
-  if (a.err_symbol_index != b.err_symbol_index)
-    return false;
-  return std::equal(a.err_symbol_coeffs.begin(), a.err_symbol_coeffs.end(),
-                    b.err_symbol_coeffs.begin());
+  // if (a.c_value != b.c_value)
+  //   return false;
+  // if (a.err_symbol_index != b.err_symbol_index)
+  //   return false;
+  // return std::equal(a.err_symbol_coeffs.begin(), a.err_symbol_coeffs.end(),
+  //                   b.err_symbol_coeffs.begin());
+  if (a.get_range().start == b.get_range().start &&
+      a.get_range().end == b.get_range().end)
+    return true;
+  return false;
 }
 
 bool operator!=(const Var &a, const Var &b) { return !(a == b); }
