f6be8e49eb579c9b428fbb691f9238cd4b587306
lauraapsailaa <laura.psaila@mail.polimi.it>
2026-08-02 17:24:45 +0200

    Revert taffo-opt/taffo-opt.cpp to match main

--- taffo-opt/taffo-opt.cpp
+++ taffo-opt/taffo-opt.cpp
@@ -1,34 +1,25 @@
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/MLIRContext.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/InitAllPasses.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Pass/PassManager.h"
 #include "mlir/Support/FileUtilities.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
-#include "llvm/Support/CommandLine.h"
-#include "llvm/Support/InitLLVM.h"
-#include "llvm/Support/SourceMgr.h"
-#include "llvm/Support/ToolOutputFile.h"
-#include "mlir/Dialect/Func/IR/FuncOps.h"
-#include "mlir/Dialect/Arith/IR/Arith.h"
-#include "mlir/Dialect/SCF/IR/SCF.h"
-#include "mlir/Dialect/MemRef/IR/MemRef.h"
-#include "mlir/Dialect/Affine/IR/AffineOps.h"
-#include "mlir/Dialect/Math/IR/Math.h"
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/InitLLVM.h>
+#include <llvm/Support/SourceMgr.h>
+#include <llvm/Support/ToolOutputFile.h>
 
 #include "Taffo/Dialect/Taffo.h"
 #include "Taffo/Transforms/Passes.h"
 
 int main(int argc, char **argv) {
-  mlir::DialectRegistry registry;
+  mlir::registerAllPasses();
 
-  registry.insert<
-      mlir::taffo::TaffoDialect,
-      mlir::func::FuncDialect,
-      mlir::arith::ArithDialect,
-      mlir::math::MathDialect,
-      mlir::scf::SCFDialect,
-      mlir::memref::MemRefDialect,
-      mlir::affine::AffineDialect>();
+  mlir::DialectRegistry registry;
+  mlir::registerAllDialects(registry);
+  registry.insert<mlir::taffo::TaffoDialect>();
 
   mlir::taffo::registerTaffoPasses();
 
